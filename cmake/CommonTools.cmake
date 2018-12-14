679ab619843048771b738255fa44175d6b66542d
Mike <no-reply1111123123123123@github.com>
2018-12-14 18:43:38 -0500

    DEV - add coverage and add utility function

--- cmake/CommonTools.cmake
+++ cmake/CommonTools.cmake
@@ -24,3 +24,18 @@ function(GET_CURRENT_FOLDER_NAME folderName)
     string(TOLOWER ${localStr} localStr)
     set(${folderName} ${localStr} PARENT_SCOPE)
 endfunction()
+
+
+# TARGET_LINK_EXT_LIBRARIES
+# ------------------------
+#
+# A helper function to suppress 3rd compile and linking warnings
+
+function(TARGET_LINK_EXT_LIBRARIES target)
+    set(libs ${ARGN})
+    foreach(lib ${libs})
+        get_target_property(lib_include_dirs ${lib} INTERFACE_INCLUDE_DIRECTORIES)
+        target_include_directories(${target} SYSTEM PUBLIC ${lib_include_dirs})
+        target_link_libraries(${target} PUBLIC ${lib})
+    endforeach(lib)
+endfunction(TARGET_LINK_EXT_LIBRARIES)
